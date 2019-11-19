eb6fd9444075cde12aab1e3acefaad7ded89a632
t.shida <tomoaki_shida@sezax.co.jp>
2019-11-19 15:33:05 +0700

    update: update dashboard styling

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import PeopleIcon from '@material-ui/icons/People';
 
 class Dashboard extends React.Component {
   render() {
     return (
-      <List>
-        <div className='app-main'>
-          <Link to="/daily_work_logs">Go To Daily Work Logs</Link>
-          <br />
-          <Link to="/users">Go to User List</Link>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+          </Toolbar>
+        </AppBar>
+        <div>
+          <Divider />
+          <List>
+            <ListItem button key='daily_work_logs'>
+              <ListItemIcon><AssignmentIcon /></ListItemIcon>
+              <Link to="/daily_work_logs">Daily Work Logs</Link>
+            </ListItem>
+            <ListItem button key='users'>
+              <ListItemIcon><PeopleIcon /></ListItemIcon>
+              <Link to="/users">Users List</Link>
+            </ListItem>
+          </List>
+          <Divider />
         </div>
-      </List>
+      </div>
     );
   }
 }
